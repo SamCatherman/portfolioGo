f224ff39860e5662ebe8bb384767d24abebb30ec
SamCatherman <sam.catherman@gmail.com>
2020-08-20 14:17:33 -0700

    article struct, mock datastore, route for articles

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,9 +14,29 @@ func homePage(w http.ResponseWriter, rt *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: return all articles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func main() {
+	Articles = []Article{
+		Article{Title: "Article 1", Desc: "first description", Content: "Blah Blah"},
+		Article{Title: "Article 2", Desc: "second description", Content: "Blah Blah"},
+	}
 	handleRequests()
 }
+
+// Article is...
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"description"`
+	Content string `json:"content"`
+}
+
+// Articles is ...
+var Articles []Article
